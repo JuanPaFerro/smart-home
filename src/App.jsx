6dc39ebd2c33bd92a49031eb50002125abeecf31
JuanPaFerro <juanpabloferroarroyo@gmail.com>
2022-07-04 09:49:36 -0400

    Created the Modal layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Modal from "./components/Modal";
 
 const App = () => {
-  return <div className="grid place-items-center bg-plant bg-center bg-cover box-border min-h-screen w-full p-8 lg:p-0"></div>;
+  return (
+    <div className="grid place-items-center bg-plant bg-center bg-cover box-border min-h-screen w-full p-8 lg:p-0">
+      <Modal />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div className="glassmorphism flex flex-col p-8 lg:p-16 box-border max-w-screen-lg w-full lg:w-8/12">
+        
+    </div>
+  )
+}
+
+export default Modal
