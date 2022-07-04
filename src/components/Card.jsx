5b8614ca1cc0fc6fb962fa236a8ddffa3daf6d16
JuanPaFerro <juanpabloferroarroyo@gmail.com>
2022-07-04 10:37:12 -0400

    Used the Switch component from HeadlessUI on the cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Switch } from "@headlessui/react";
 
 const Card = ({ title, iconURL }) => {
+  const [enabled, setEnabled] = useState(false);
+
   return (
     <div className="glassmorphism relative grid place-items-center py-3 box-border w-full h-full cursor-pointer">
       <p className="m-0 text-sm">{title}</p>
       <img className="w-8 m-2" src={iconURL} />
-      <input
-        id="lights-toggle-button"
-        type="checkbox"
-        className="absolute w-full h-full opacity-0"
-      />
-      <label
-        for="lights-toggle-button"
-        className="flex items-center bg-gray-400 h-6 w-12 rounded-full"
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={`${
+          enabled ? "bg-green-500" : "bg-gray-200"
+        } relative inline-flex h-6 w-11 items-center rounded-full`}
       >
-        <div className="toggle-dot bg-white m-1 h-5 w-5 rounded-full"></div>
-      </label>
+        <span
+          className={`${
+            enabled ? "translate-x-6" : "translate-x-1"
+          } inline-block h-4 w-4 transform rounded-full bg-white transition ease-in-out duration-200`}
+        />
+      </Switch>
     </div>
   );
 };
