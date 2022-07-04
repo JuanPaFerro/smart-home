64d4414face6f7a3d3e6f2fde9482db50cf90426
JuanPaFerro <juanpabloferroarroyo@gmail.com>
2022-07-04 09:46:54 -0400

    Created the layout and added a background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className=""></div>;
+  return <div className="grid place-items-center bg-plant bg-center bg-cover box-border min-h-screen w-full p-8 lg:p-0"></div>;
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        plant:
+          "url('https://images.pexels.com/photos/1571458/pexels-photo-1571458.jpeg')",
+      },
+    },
   },
   plugins: [],
 };
