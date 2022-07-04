5c706e11fa256d002c0a93044cc60dc2f489418f
JuanPaFerro <juanpabloferroarroyo@gmail.com>
2022-07-04 09:51:13 -0400

    Added the title to the Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Modal = () => {
   return (
     <div className="glassmorphism flex flex-col p-8 lg:p-16 box-border max-w-screen-lg w-full lg:w-8/12">
-        
+      <p class="text-base lg:text-xl mt-0 mb-2 text-center">
+        You are controlling
+      </p>
+      <h1 class="text-2xl lg:text-4xl m-0 text-center font-bold">
+        {"Main Lounge & Dining Room"}
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
