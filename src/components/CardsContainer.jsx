a19b961ef4790b4608c09a08d87f2e3fed47f351
JuanPaFerro <juanpabloferroarroyo@gmail.com>
2022-07-04 10:15:07 -0400

    Created the container for the cards

---
+++ src/components/CardsContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CardsContainer = () => {
+  return (
+    <div class="controls grid place-items-center w-full lg:w-2/6 mt-8 lg:my-0"></div>
+  );
+};
+
+export default CardsContainer;
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CardsContainer from "./CardsContainer";
 
 const Modal = () => {
   return (
@@ -17,6 +18,7 @@ const Modal = () => {
             alt="plan"
           />
         </div>
+        <CardsContainer />
       </div>
     </div>
   );
